374592764637ccb2832e2e5c4e8db0a9bcfe5d38
hvelarde <hector.velarde@gmail.com>
2011-12-24 10:37:14 -0600

    fixed i18n script

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -6,7 +6,7 @@ I18NDOMAIN="collective.polls"
 # All on one line normally:
 bin/i18ndude rebuild-pot --pot src/collective/polls/locales/${I18NDOMAIN}.pot \
     --create ${I18NDOMAIN} \
-   .
+    src/collective/polls
 
 # Synchronise the resulting .pot with all .po files
 for po in src/collective/polls/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
