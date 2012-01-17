8499b5aa0ef035b61e49168be3b5399ecaee4e38
hvelarde <hector.velarde@gmail.com>
2012-01-16 19:53:05 -0600

    included a report of errors and suspect untranslated messages

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -1,14 +1,17 @@
 #! /bin/sh
 
 I18NDOMAIN="collective.polls"
+BASE_DIRECTORY="src/collective/polls"
 
 # Synchronise the templates and scripts with the .pot.
-# All on one line normally:
-bin/i18ndude rebuild-pot --pot src/collective/polls/locales/${I18NDOMAIN}.pot \
+bin/i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
     --create ${I18NDOMAIN} \
-    src/collective/polls
+    ${BASE_DIRECTORY}
 
 # Synchronise the resulting .pot with all .po files
-for po in src/collective/polls/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
-    bin/i18ndude sync --pot src/collective/polls/locales/${I18NDOMAIN}.pot $po
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    bin/i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
 done
+
+# Report of errors and suspect untranslated messages
+bin/i18ndude find-untranslated ${BASE_DIRECTORY}
