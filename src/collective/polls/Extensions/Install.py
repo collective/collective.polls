44f891fcfb96c655bab78f7f80d9e9f82be02947
hvelarde <hector.velarde@gmail.com>
2012-01-16 21:52:46 -0600

    fixed javascript registry uninstall and tests

--- src/collective/polls/Extensions/Install.py
+++ src/collective/polls/Extensions/Install.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from Products.CMFCore.utils import getToolByName
 
+from collective.polls.config import PROJECTNAME
+
+
 def uninstall(portal):
+    profile = 'profile-%s:uninstall' % PROJECTNAME
     setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile('profile-collective.polls:uninstall')
+    setup_tool.runAllImportStepsFromProfile(profile)
     return "Ran all uninstall steps."
--- src/collective/polls/tests/test_setup.py
+++ src/collective/polls/tests/test_setup.py
@@ -3,8 +3,6 @@
 import unittest2 as unittest
 
 from plone.app.testing import TEST_USER_ID
-from plone.app.testing import TEST_USER_NAME
-from plone.app.testing import login
 from plone.app.testing import setRoles
 
 from collective.polls.config import PROJECTNAME
@@ -50,12 +48,11 @@ class UninstallTest(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        login(self.portal, TEST_USER_NAME)
+        self.qi = getattr(self.portal, 'portal_quickinstaller')
+        self.qi.uninstallProducts(products=[PROJECTNAME])
 
     def test_uninstalled(self):
-        qi = getattr(self.portal, 'portal_quickinstaller')
-        qi.uninstallProducts(products=[PROJECTNAME])
-        self.assertFalse(qi.isProductInstalled(PROJECTNAME))
+        self.assertFalse(self.qi.isProductInstalled(PROJECTNAME))
 
     def test_jsregistry_removed(self):
         portal_javascripts = self.portal.portal_javascripts
