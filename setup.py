f56a7c3bab2865e83914db252c749f1257dd63ec
hvelarde <hector.velarde@gmail.com>
2012-09-16 22:18:58 -0300

    bump up collective.z3cform.widgets dependency version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(name='collective.polls',
         'Products.CMFPlone>=4.1',
         'plone.app.dexterity>=1.2.1',
         'plone.app.referenceablebehavior',
-        'collective.z3cform.widgets>=1.0a4',
+        'collective.z3cform.widgets>=1.0b1',
         ],
       extras_require={
         'test': ['plone.app.testing'],
