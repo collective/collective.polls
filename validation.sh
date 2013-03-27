62b89981b2f76ce0ee4f5a9387bfac3b4ed264fd
hvelarde <hector.velarde@gmail.com>
2013-03-27 11:52:21 -0300

    fix validation script

--- validation.sh
+++ validation.sh
@@ -8,17 +8,17 @@
 # CSS_IGNORES: skip file names matching find pattern (use ! -name PATTERN)
 # JS_IGNORES: skip file names matching find pattern (use ! -name PATTERN)
 
-SRC = src/collective/polls/
-MINIMUM_COVERAGE = 72
-PEP8_IGNORES = E501
-MAX_COMPLEXITY = 12
-CSS_IGNORES = ! -name jquery\*
-JS_IGNORES = ! -name excanvas\* ! -name jquery\*
+SRC=src/collective/polls/
+MINIMUM_COVERAGE=72
+PEP8_IGNORES=E501
+MAX_COMPLEXITY=12
+CSS_IGNORES="! -name jquery\*"
+JS_IGNORES ="! -name excanvas\* ! -name jquery\*"
 
 if "$QA" != "" ; then
 	@echo Validating Python files
 	# FIXME: skip complexity validation for now as we don't have time to deal with it
-	#bin/flake8 --ignore=$(PEP8_IGNORES) --max-complexity=$(MAX_COMPLEXITY) $(SRC)
+	#bin/flake8 --ignore=$PEP8_IGNORES --max-complexity=$MAX_COMPLEXITY $SRC
 	bin/flake8 --ignore=$PEP8_IGNORES $SRC
 
 	@echo Validating minimun test coverage
