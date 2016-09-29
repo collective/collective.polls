3e3d17eef6bbd11ab08aa147bac4b7d5b477fc17
Héctor Velarde <hvelarde@yahoo.com>
2016-09-29 14:17:26 -0300

    Skip RobotFramework tests under Plone 4.2 (#104)

--- src/collective/polls/tests/test_robot.py
+++ src/collective/polls/tests/test_robot.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 from collective.polls.testing import ROBOT_TESTING
+from plone import api
 from plone.testing import layered
 
 import os
 import robotsuite
 import unittest
 
+PLONE_VERSION = api.env.plone_version()
+IS_PLONE_42 = PLONE_VERSION.startswith('4.2')
+
 dirname = os.path.dirname(__file__)
 files = os.listdir(dirname)
 tests = [f for f in files if f.startswith('test_') and f.endswith('.robot')]
+
+# skip RobotFramework tests in Plone 4.2
+# FIXME: https://github.com/collective/collective.polls/issues/103
+if IS_PLONE_42:
+    tests = []
 
 
 def test_suite():
