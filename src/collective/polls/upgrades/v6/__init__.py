43692356e8c33f1ec79dcd3d6be5579fbb526236
Héctor Velarde <hvelarde@yahoo.com>
2017-06-13 23:14:19 -0300

    Remove ExplorerCanvas script (#119)

--- src/collective/polls/upgrades/v6/__init__.py
+++ src/collective/polls/upgrades/v6/__init__.py
@@ -10,3 +10,12 @@ def register_tasksplease_script(setup_tool):
     js_tool.registerResource(script)
     assert script in js_tool.getResourceIds()
     logger.info('TasksPlease script registered')
+
+
+def remove_excanvas_script(setup_tool):
+    """Remove script used to add support for the canvas tag on IE8 and below."""
+    script = '++resource++collective.polls/js/excanvas.min.js'
+    js_tool = api.portal.get_tool('portal_javascripts')
+    js_tool.unregisterResource(script)
+    assert script not in js_tool.getResourceIds()
+    logger.info('ExplorerCanvas script removed')
